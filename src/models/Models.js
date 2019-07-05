c9d76288051f8625f6c6e1eb02c66c283e0dcc0f
PenCoder <clus@PECLUS.kuikdelivery.com>
2019-07-05 16:50:25 +0000

    all

---
+++ src/models/Models.js
@@ -0,0 +1,19 @@
+export default class PersonalDetails {
+    
+    dob= null;
+    gender= null;
+    maritalStatus= null
+    identification= null;
+    nationality= null;
+    tin= null;
+    contact= {
+        mobile: null,
+        email: null,
+        postal: null,
+        digitalAddress: null,
+        street: null,
+        suburb: null,
+        town: null, 
+        region: null
+    }
+}
